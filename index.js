65676bc6381ebe3b0578568c84f7020d1a005dfb
Syechran <akusyechran@gmail.com>
2025-02-15 08:17:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,28 @@ const API_URL = process.env.API_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_KEY;
 
-const{}
+const { abi } = require('./artifacts/contracts/TaskToDo.sol/TaskToDo.json');
+const provider = new ethers.provider.JsonRpcProvider(API_URL);
+
+const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+
+const contractInstance = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+app.post("/changeStatus", async (req, res) => {
+    var id = req.body.id;
+
+    async function storeDataInBlockchain(id) {
+        console.log("Changing the task status...");
+        const tx = await contractInstance.markAsFinished(id);
+        await tx.wait();
+    }
+
+    await storeDataInBlockchain(id);
+
+    res.send("The task has been changed in the smart contract");
+});
+
+app.listen(port, function() {
+  console.log("App is listening on port 3000");
+});
+
