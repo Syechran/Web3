669238b267a8b88c3dfb8a888f86212eef16f7d4
Rizkii-dev <66073041+Rizkii-dev@users.noreply.github.com>
2025-02-16 09:49:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-app.use(express.static(_dirname));
+const fileUpload = require('express-fileupload');
+app.use(
+    fileUpload({
+        extended:true
+    })
+)
+app.use(express.static(__dirname));
 app.use(express.json());
 const path = require("path");
 const ethers = require('ethers');
 
 var port = 3000;
 
-app.get("/",(req,res) => {
-  res.sendFile(path.join(_dirname."index.html"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 })
 
-app.get("/index.html", (req,res) => {
-  res.sendFile(path.join(_dirname,"index.html"));
+app.get("/index.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 })
+
 
 const API_URL = process.env.API_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const CONTRACT_ADDRESS = process.env.CONTRACT_KEY;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
-const { abi } = require('./artifacts/contracts/TaskToDo.sol/TaskToDo.json');
-const provider = new ethers.provider.JsonRpcProvider(API_URL);
+const {abi} = require('./artifacts/contracts/TaskToDo.sol/TaskToDo.json');
+const provider = new ethers.providers.JsonRpcProvider(API_URL);
 
 const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 
 const contractInstance = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+
+app.post("/addTask", async (req, res) => {
+    var task = req.body.task;
+    console.log(task)
+    async function storeDataInBlockchain(task) {
+        console.log("Adding the task in the blockchain network...");
+        const tx = await contractInstance.addTask(task);
+        await tx.wait();
+    }
+
+    await storeDataInBlockchain(task);
+    res.send("The task has been registered in the smart contract");
+});
+
 
 app.post("/changeStatus", async (req, res) => {
     var id = req.body.id;
@@ -37,11 +59,10 @@ app.post("/changeStatus", async (req, res) => {
     }
 
     await storeDataInBlockchain(id);
-
-    res.send("The task has been changed in the smart contract");
+    res.send("The task status has been changed in the smart contract");
 });
 
-app.listen(port, function() {
-  console.log("App is listening on port 3000");
-});
 
+app.listen(port, function () {
+    console.log("App is listening on port 3000")
+});
